757511315e4087537940e081494e44a5f1c6ed99
t-miyajima <mail@t-miyajima.jp>
2024-03-17 22:30:00 +0900

    vial firmware almost done

---
+++ firmware/keymaps/vial/config.h
@@ -0,0 +1,29 @@
+/* Copyright 2020 t-miyajima
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// place overrides here
+#define VIAL_KEYBOARD_UID {0x58, 0xDB, 0x14, 0xB4, 0x67, 0x79, 0x52, 0xCB}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
+// #define FEE_MCU_FLASH_SIZE 32
+// #define FEE_PAGE_COUNT 2
+// #define FEE_PAGE_SIZE 950
+// #define LAYER_STATE_8BIT
+// #define NO_ACTION_ONESHOT
+// #undef LOCKING_SUPPORT_ENABLE
+// #undef LOCKING_RESYNC_ENABLE
